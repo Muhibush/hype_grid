c0c0b3d914b7034f1aa563cc700791ce68b3af60
muhibush <ukiacrew@gmail.com>
2026-03-12 23:33:07 +0700

    refactor: Remove Firebase Cloud Messaging functionality and consolidate Firebase configuration by directly including `google-services.json`.

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,10 @@ import 'package:hype_grid/utils/app_theme.dart';
 import 'package:hype_grid/pages/splash/splash_screen.dart';
 import 'package:hype_grid/services/supabase_service.dart';
 import 'package:hype_grid/pages/detail/event_detail_screen.dart';
-import 'package:hype_grid/services/notification_service.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'package:hype_grid/utils/environment.dart';
-import 'package:hype_grid/firebase_options_dev.dart' as dev;
-import 'package:hype_grid/firebase_options_prod.dart' as prod;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,65 +27,15 @@ Future<void> main() async {
   // Initialize Supabase
   await SupabaseService().initialize();
 
-  // Initialize Firebase (using aliased options)
-  try {
-    final firebaseOptions = Environment.isProd
-        ? prod.DefaultFirebaseOptions.currentPlatform
-        : dev.DefaultFirebaseOptions.currentPlatform;
-
-    await Firebase.initializeApp(
-      options: firebaseOptions,
-    );
-    await NotificationService().initialize();
-  } catch (e) {
-    debugPrint('Firebase initialization failed: $e');
-  }
-
   runApp(const HypeGridApp());
 }
 
-class HypeGridApp extends StatefulWidget {
+class HypeGridApp extends StatelessWidget {
   const HypeGridApp({super.key});
-
-  @override
-  State<HypeGridApp> createState() => _HypeGridAppState();
-}
-
-class _HypeGridAppState extends State<HypeGridApp> {
-  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
-
-  @override
-  void initState() {
-    super.initState();
-    _setupInteractedMessage();
-  }
-
-  Future<void> _setupInteractedMessage() async {
-    // Get any messages which caused the application to open from
-    // a terminated state.
-    RemoteMessage? initialMessage = await FirebaseMessaging.instance
-        .getInitialMessage();
-
-    if (initialMessage != null) {
-      _handleMessage(initialMessage);
-    }
-
-    // Also handle any interaction when the app is in the background via a
-    // Stream listener
-    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
-  }
-
-  void _handleMessage(RemoteMessage message) {
-    if (message.data.containsKey('event_id')) {
-      final eventId = message.data['event_id'];
-      _navigatorKey.currentState?.pushNamed('/event/$eventId');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      navigatorKey: _navigatorKey,
       title: 'HypeGrid',
       theme: AppTheme.darkTheme,
       home: const SplashScreen(),
